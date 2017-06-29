ef21739744baa19604cef15cf832e2494a7a37a1
ronald132 <ronald132@gmail.com>
2017-06-29 17:36:27 +0800

    make it better

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejs_mate = require('ejs-mate');
+
+var app = express();
+
+app.engine('ejs', ejs_mate);
+app.set('view engine', 'ejs');
+
+app.use(require('./routes/main'));
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function(){
+  console.log("listening on port 3000");
+});
