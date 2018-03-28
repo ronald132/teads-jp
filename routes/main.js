7462a132f834b8077c71901f65d889993b8b603f
Ronald Situmorang <ronald132@gmail.com>
2018-03-28 18:12:57 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -7,5 +7,8 @@ router.get('/', function(req, res){
 router.get('/ctr_test', function(req, res){
   res.render('ctr');
 });
+router.get('/dbm_ctr', function(req, res){
+  res.render('dbm_ctr');
+});
 
 module.exports = router;
