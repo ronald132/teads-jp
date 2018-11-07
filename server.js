5cb1c8ec473791ca5fdac6f3e58d2ef763135bf4
Ronald Situmorang <ronald132@gmail.com>
2018-11-07 11:38:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 app.engine('ejs', ejs_mate);
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(require('./routes/main'));
 
 app.set('port', process.env.PORT || 3000);
